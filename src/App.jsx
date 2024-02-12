79346b68b9270c808a5e888e5cd12b617fa5b688
toyyibah <toyyibat.abdulkareem@gmail.com>
2024-02-12 12:45:16 +0100

    added folder sturcture

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import Routers from './Routers/Routers'
+import './App.css';
+
+function App() {
+  return (
+    <div style={{textAlign:'center', width:'100vw'}}>
+    <h1>Connect with Toyyibah</h1>
+  {/* <Routers /> */}
+  </div>
+  );
+}
+
+export default App;
---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './signUp.module.css'
+
+const index = () => {
+  return (
+    <div>
+        SignUp
+    </div>
+  )
+}
+
+export default index
